8a26539208f35626f481fa2b8454d8400882a9f7
6k <6k@iMac-de-iMac10.local>
2017-03-22 11:49:41 -0600

    Agregando casos de prueba

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -24,6 +24,16 @@ void casos_de_prueba(){
 
     cout << "TEST CASES" << endl;
     // Agrega tus casos de prueba
+    
+    cout << "Test data : 1 / Expeded Result: 10 / Actual Result: "<< pago_estacionamiento(1)<<endl;
+    cout << "Test data : 5 / Expeded Result: 50 / Actual Result: "<< pago_estacionamiento(5)<<endl;
+    cout << "Test data : 6 / Expeded Result: 54 / Actual Result: "<< pago_estacionamiento(6)<<endl;
+    cout << "Test data : 10 / Expeded Result: 90 / Actual Result: "<< pago_estacionamiento(10)<<endl;
+    cout << "Test data : 11/ Expeded Result: 77 / Actual Result: "<< pago_estacionamiento(11)<<endl;
+    cout << "Test data : 0 / Expeded Result: 0 / Actual Result: "<< pago_estacionamiento(0)<<endl;
+    cout << "Test data : 4 / Expeded Result: 40 / Actual Result: "<< pago_estacionamiento(4)<<endl;
+    cout << "Test data : 8 / Expeded Result: 72 / Actual Result: "<< pago_estacionamiento(8)<<endl;
+    cout << "Test data : 13 / Expeded Result: 91 / Actual Result: "<< pago_estacionamiento(13)<<endl;
 
 
 
